e9ed2e7859434ca6caae07fb674bca28fa81130b
LouaiAloui <louai.aloui@esprit.tn>
2022-12-01 17:23:19 +0100

    -unit tests for the services to ensure we got defined functions - now the next step will be server-side-rendering

--- server/tests/stats.test.js
+++ server/tests/stats.test.js
@@ -1,16 +1,12 @@
-const { expect } = require('chai');
 const { averageRoundsLength, killsForEachPlayer, scoreboardOverTime } = require('../services/stats.service');
-
-describe('Stats test', () => {
-    it('Checking the return data to be float since we have average', () => {
-        expect(averageRoundsLength).to.be.true;
-    });
-
-    it('kills per player should return string', () => {
-        expect(killsForEachPlayer).to.be.an('string');
-    });
-
-    it('scoreboard over time should return string', () => {
-        expect(scoreboardOverTime).to.be.an('String');
+describe('Testing endpoints services', () => {
+    it('should return defined killers per player', async () => {
+        expect(killsForEachPlayer).toBeDefined()
     })
-});
+    it('should return defined scorebord', async () => {
+        expect(scoreboardOverTime).toBeDefined()
+    })
+    it('should return defined average round length', async () => {
+        expect(averageRoundsLength).toBeDefined()
+    })
+})
